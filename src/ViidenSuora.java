439f718152f5023e145fa8c006d35297e5c4cdc4
macjantu <macjantu@macjantu-System-Product-Name>
2012-08-01 22:32:24 +0300

    Testi

--- src/ViidenSuora.java
+++ src/ViidenSuora.java
@@ -5,5 +5,7 @@ public class ViidenSuora {
     public static void main(String[] args) {
         System.out.println("Tämä on viiden suora!");
         System.out.println("Testailen vaan näkyykö tää file gitissä.");
+        
+        System.out.println("Toinen pushaustesti");
     }
 }
