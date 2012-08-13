ce63365ad25dab7b051dba65275a916ae9fea588
Maaku <Maaku@Maaku-Main>
2012-08-14 02:19:11 +0300

    9.8 @2:23 perinteinen ristinolla toimii nyt kaksinpelinä, uudistettu rankalla kädellä koko toimintaa

--- src/Testi.java
+++ src/Testi.java
@@ -8,49 +8,52 @@
  * @author emmivesa
  */
 public class Testi {
-    
-    private Node solmu;
-    private Puu puu;
-    int rivi;   //ei tarvita mutta jätin viel hetkeks kun ton rekursio-
-    int sarake; //puun talletus viel vaiheessa.
-    private Peli todellinen; //nimi pitänee vaihtaa
-    private char pelaaja; //HUOM! en ole tehnyt vielä niin, että vuoron mukaan
-                           //vaihtuisi X ja O
-    
-    public Testi() {
-        this.rivi = 0;
-        this.sarake = 0;
-    }
-    
-    public Testi(int rivi, int sarake) {
-        this.rivi = rivi;
-        this.sarake = sarake;
-    }
-    public void asetaMatriisiin() {
+
+    private Peli palautettava;//paras tilanne
+    private Peli todellinen; //=pelitilanne
+    private char pelaaja;
+    int myBest;
+
+    public Testi(Peli tilanne) {
+        this.todellinen = tilanne;
+        this.palautettava = todellinen;
+        this.pelaaja = 'X';
         
     }
-    /**
-     * 
-     */
-    public void kayLapi() { //tänne tullaan jostain päämetodista
-        for(int r=0; r < todellinen.KOKO; r++) {
-            for(int s=0; s<todellinen.KOKO; s++) {
-                if(todellinen.pelilauta[r][s] == todellinen.TYHJA) {
-                    todellinen.setRuutu(r, s, pelaaja);
-                    kayLapi();
-                }
-                //tehdään tarkistukset ollaanko reunoissa tai lopussa,
-                //ja muutetaan sen mukaan r(=riviä) ja s(=saraketta), todnäk
-                //pitää tehdä jo ennen tota if:iä.
-                else if(r == todellinen.KOKO-1 && s < todellinen.KOKO-1) {//ollaanko oikeassa reunassa muttei vikassa
-                    //koska ollaan täällä, on reunassa jo merkki.
-                    rivi++;
-                    sarake=0;
-                    kayLapi();
-                }
+
+    public void tarkistaTilanne() {
+        kayLapi();
+    }
+
+    public int kayLapi() { 
+
+        for (int r = 0; r < todellinen.KOKO; r++) {
+            for (int s = 0; s < todellinen.KOKO; s++) {
+
+                if (palautettava.voittiko() == 'X') {
+                    return 1; //return
+                } else if (palautettava.voittiko() == '0') {
+                    return -1; //return
+                } else if (palautettava.voittiko() == 't') {
+                    return 0; //return
                 
+                } else {
+
+                    if (palautettava.pelilauta[r][s] == todellinen.TYHJA) {
+                        palautettava.setRuutu(r, s, pelaaja);
+                        if (this.pelaaja == 'X') {
+                            pelaaja = '0';
+                        } else {
+                            pelaaja = 'X';
+                        }
+                        kayLapi();
+                        
+                    } else {
+                        kayLapi();
+                    }
+                }
             }
         }
+        return 88;
     }
-    
 }
