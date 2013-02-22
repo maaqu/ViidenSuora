e324286dc3a6a9a08421559116d48209d5f6a5e9
maaqu <maa_q@hotmail.com>
2013-02-22 04:49:56 +0200

    Update src/ViidenSuora.java

--- src/ViidenSuora.java
+++ src/ViidenSuora.java
@@ -1,15 +1,9 @@
-
 public class ViidenSuora {
 
 
     public static void main(String[] args) {
-        System.out.println("Tämä on viiden suora!");
 
-        
-       char[][] lauta = { {'O', 'O', ' '}, {'O', 'X', 'X'}, {' ', 'X', 'X'} }; //testailua
-        
         Peli ristinolla = new Peli();
-//        ristinolla.tulosta(1);
         
         ristinolla.pelaa();
          
